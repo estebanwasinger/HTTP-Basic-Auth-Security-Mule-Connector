ebbe95974183aa7423c6aeb2d1e8ca0ff2cba4c5
estebanwasinger <estebanwasinger@gmail.com>
2016-07-21 22:04:00 -0300

    Fix message event control

--- src/main/java/org/mule/modules/basicauthsecurity/BasicAuthSecurityConnector.java
+++ src/main/java/org/mule/modules/basicauthsecurity/BasicAuthSecurityConnector.java
@@ -1,4 +1,4 @@
-/**
+/*
  * (c) 2003-2015 MuleSoft, Inc. The software in this package is published under the terms of the CPAL v1.0 license,
  * a copy of which has been included with this distribution in the LICENSE.md file.
  */
@@ -7,7 +7,6 @@ package org.mule.modules.basicauthsecurity;
 
 import org.apache.log4j.Logger;
 import org.mule.api.MuleEvent;
-import org.mule.api.MuleMessage;
 import org.mule.api.annotations.Category;
 import org.mule.api.annotations.Config;
 import org.mule.api.annotations.Connector;
@@ -29,46 +28,45 @@ import java.util.Map;
 @Category(name = "org.mule.tooling.category.security", description = "Security")
 @Connector(name = "basic-auth-security", friendlyName = "HTTP Basic Auth Security")
 public class BasicAuthSecurityConnector {
-	
-	private static final Logger logger = Logger.getLogger(BasicAuthSecurityConnector.class);
 
-	@Config
-	SecurityProvider connectionStrategy;
-	
-	@Processor(intercepting = true)
-	public Object secure(SourceCallback callback, MuleEvent me, MuleMessage mm, List<String> acceptedRoles) throws Exception  {
-		Map<String, Object> mapa = new HashMap<String, Object>();
-		String authorization = null;
-		try {
-			String auth = me.getMessage().getInboundProperty("authorization");
-			
-			authorization = new String(Base64.decode(auth.substring(6)));
-			getConnectionStrategy().validate(authorization,acceptedRoles);
-			
-			logger.info(authorization);
-			for(String role : acceptedRoles){
-				logger.info(role);
-			}
-		} catch (Exception e) {
-			mapa.put("WWW-Authenticate", "Basic realm=\"Unathorized\"");
-			mapa.put("Content-Length", 0);
-			mapa.put("http.status", 401);
-			me.getMessage().addProperties(mapa, PropertyScope.OUTBOUND);
-			return me.getMessage().getPayload();
-		}
-		mapa.put("auth", authorization);
-		me.getMessage().addProperties(mapa, PropertyScope.OUTBOUND);
-		callback.process(me.getMessage().getPayload());
-		return me.getMessage().getPayload();
-	}
+    private static final Logger logger = Logger.getLogger(BasicAuthSecurityConnector.class);
 
-	public SecurityProvider getConnectionStrategy() {
-		return connectionStrategy;
-	}
+    @Config
+    SecurityProvider connectionStrategy;
 
-	public void setConnectionStrategy(
-			SecurityProvider connectionStrategy) {
-		this.connectionStrategy = connectionStrategy;
-	}
+    @Processor(intercepting = true)
+    public MuleEvent secure(SourceCallback callback, MuleEvent muleEvent, List<String> acceptedRoles) throws Exception {
+        Map<String, Object> map = new HashMap<>();
+        String authorization;
+        try {
+            String auth = muleEvent.getMessage().getInboundProperty("authorization");
+
+            authorization = new String(Base64.decode(auth.substring(6)));
+            getConnectionStrategy().validate(authorization, acceptedRoles);
+
+            logger.info(authorization);
+            for (String role : acceptedRoles) {
+                logger.info(role);
+            }
+        } catch (Exception e) {
+            map.put("WWW-Authenticate", "Basic realm=\"Unauthorized\"");
+            map.put("Content-Length", 0);
+            map.put("http.status", 401);
+            muleEvent.getMessage().addProperties(map, PropertyScope.OUTBOUND);
+            return muleEvent;
+        }
+        map.put("auth", authorization);
+        muleEvent.getMessage().addProperties(map, PropertyScope.OUTBOUND);
+        return callback.processEvent(muleEvent);
+    }
+
+    public SecurityProvider getConnectionStrategy() {
+        return connectionStrategy;
+    }
+
+    public void setConnectionStrategy(
+            SecurityProvider connectionStrategy) {
+        this.connectionStrategy = connectionStrategy;
+    }
 
 }
--- src/main/java/org/mule/modules/basicauthsecurity/strategy/MemorySecurityProviderStrategy.java
+++ src/main/java/org/mule/modules/basicauthsecurity/strategy/MemorySecurityProviderStrategy.java
@@ -8,8 +8,11 @@ package org.mule.modules.basicauthsecurity.strategy;
 import org.mule.api.annotations.Configurable;
 import org.mule.api.annotations.components.Configuration;
 import org.mule.api.annotations.param.Default;
+import org.mule.api.annotations.param.Optional;
 import org.mule.modules.basicauthsecurity.UnauthorizedException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,64 +23,68 @@ import java.util.Map;
  */
 @Configuration(configElementName = "config-type", friendlyName = "Memory Security Provider Strategy")
 public class MemorySecurityProviderStrategy implements SecurityProvider {
-	
-	@Configurable
-	@Default("false")
-	public Boolean usersAsMap;
 
-	public Boolean getUsersAsMap() {
-		return usersAsMap;
-	}
+    @Configurable
+    @Default("false")
+    private Boolean usersAsMap;
 
-	public void setUsersAsMap(Boolean usersAsMap) {
-		this.usersAsMap = usersAsMap;
-	}
+    @Configurable
+    @Optional
+    private
+    List<String> users = new ArrayList<>();
 
-	@Configurable
-	List<String> users;
+    @Configurable
+    @Optional
+    private
+    Map<String, String> usersMap = new HashMap<>();
 
-	public List<String> getUsers() {
-		return users;
-	}
+    public void validate(String auth, List<String> acceptedRoles) throws UnauthorizedException {
+        if (usersAsMap) {
+            String password = usersMap.get(getUser(auth));
+            if (password == null || !password.equals(getPass(auth))) {
+                throw new UnauthorizedException(auth.split(":")[0]);
+            }
+        } else {
+            for (String userInMemory : users) {
+                if (!(getUser(auth).equals(getUser(userInMemory))
+                        && getPass(auth).equals(getPass(userInMemory)))) {
+                    throw new UnauthorizedException(auth.split(":")[0]);
+                }
+            }
+        }
+    }
 
-	public void setUsers(List<String> users) {
-		this.users = users;
-	}
-	
-	@Configurable
-	Map<String, String> usersMap;
-	
+    private String getUser(String auth) {
+        return auth.split(":")[0];
+    }
 
-	public Map<String, String> getUsersMap() {
-		return usersMap;
-	}
+    private String getPass(String auth) {
+        return auth.split(":")[1];
+    }
 
-	public void setUsersMap(Map<String, String> usersMap) {
-		this.usersMap = usersMap;
-	}
+    public Boolean getUsersAsMap() {
+        return usersAsMap;
+    }
 
-	public void validate(String auth, List<String> acceptedRoles) throws UnauthorizedException {
-		if (usersAsMap) {
-			String password = usersMap.get(getUser(auth));
-			if(password == null || !password.equals(getPass(auth))){
-				throw new UnauthorizedException(auth.split(":")[0]);
-			}
-		} else {
-			for (String userInMemory : users) {
-				if (!(getUser(auth).equals(getUser(userInMemory)) && getPass(
-						auth).equals(getPass(userInMemory)))) {
-					throw new UnauthorizedException(auth.split(":")[0]);
-				}
-			}
-		}
-	}
-	
-	private String getUser(String auth){
-		return auth.split(":")[0];
-	}
-	
-	private String getPass(String auth){
-		return auth.split(":")[1];
-	}
+    public void setUsersAsMap(Boolean usersAsMap) {
+        this.usersAsMap = usersAsMap;
+    }
+
+    public List<String> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<String> users) {
+        this.users = users;
+    }
+
+    public Map<String, String> getUsersMap() {
+        return usersMap;
+    }
+
+    public void setUsersMap(Map<String, String> usersMap) {
+        this.usersMap = usersMap;
+    }
+
 
 }
